a3db2783df269027f58e66b7c9b69344dff9645c
Lino Silva <lino.silva@farfetch.com>
2019-06-17 10:19:48 +0100

    Added license, regex to clean search terms

--- background.js
+++ background.js
@@ -1,15 +1,26 @@
+/* Copyright 2019, Lino Silva
+ * You may use, distribute and modify this code under the
+ * terms of the MIT License.
+ * 
+ * It's just an extension to search for beer, lighten up a little!
+ */
 
+const CONSTANTS = {
+  baseUrl: 'https://untappd.com/search?q=',
+  trim: /\s?(can(s)?)?\s?\d+(\.\d+)?\s?([cdm]?)l$/ig // Fucking regexes.
+}
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
-    title: 'Search in untappd.com',
+    title: 'Search in Untappd',
     id: 'search-untappd',
     contexts: ['selection'],
   });
 });
 
 chrome.contextMenus.onClicked.addListener(function (info) {
-  if (info.menuItemId === "search-untappd") {
-    const url = `https://untappd.com/search?q=${encodeURIComponent(info.selectionText)}`;
+  if (info && info.selectionText && info.menuItemId === 'search-untappd') {
+    const trimmedText = info.selectionText.replace(CONSTANTS.trim, '');
+    const url = `${CONSTANTS.baseUrl}${encodeURIComponent(trimmedText)}`;
     chrome.tabs.create({ url });
   }
 });
